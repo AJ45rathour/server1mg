b145b532cde6e7701174da2fa1a94d84568b2979
AJ Rathour <ajaykumarrathour3344@gmail.com>
2025-08-25 14:29:16 +0530

    changes done to db

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,19 +1,19 @@
-// db.js
-import mysql from 'mysql';
+// // db.js
+// import mysql from 'mysql';
 
-const dbConn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: '1mg'
-});
+// const dbConn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: '1mg'
+// });
 
-dbConn.connect((err) => {
-  if (err) {
-    console.error('❌ Error connecting to MySQL:', err.message);
-    return;
-  }
-  console.log('✅ Connected to MySQL database: 1mg');
-});
+// dbConn.connect((err) => {
+//   if (err) {
+//     console.error('❌ Error connecting to MySQL:', err.message);
+//     return;
+//   }
+//   console.log('✅ Connected to MySQL database: 1mg');
+// });
 
-export default dbConn;
+// export default dbConn;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import dbConn from './config/dbConn.js';
+// import dbConn from './config/dbConn.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
